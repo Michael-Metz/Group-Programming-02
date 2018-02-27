dba9e662ed1c61a3488450c0759d51cb832730c7
MIchael Metz <msmetz@ilstu.edu>
2018-02-27 17:21:36 -0600

    Added comments to insertionsortdriver

--- InsertionSortDriver.cpp
+++ InsertionSortDriver.cpp
@@ -12,23 +12,34 @@ void printUsage(){
 }
 int main(int arc, char *argv[]) {
 
+    //make sure two arguments are  passed when running the program from command line
     if(arc != 3) {
         printUsage();
         return 0;
     }
+    //at this point
+    //argv[1] is the path to the input file
+    //argv[2] is the path to save the output file
 
     LinkedList* list = new LinkedList;
     ifstream inputFile(argv[1]);
+    
     if(!inputFile.is_open())
         cout << "could not open file" << endl;
+    else
+    {
+        //read input file line by line and add to list
+         long num;
+        while(inputFile >> num)
+           list->addTail(num);
+    
+         inputFile.close();
 
-    long data;
-    while(inputFile >> data)
-        list->addTail(data);
-    inputFile.close();
+         //sort the list
+         list->insertionSort();
 
-    list->insertionSort();
-
-    list->outputToFile(argv[2]);
+         //save list to file
+         list->outputToFile(argv[2]);
+    }
     return 0;
 }
