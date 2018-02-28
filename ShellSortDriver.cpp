146b2bbf58da61ac93a26614bbccee5da0b5aa8b
Dean Schulz <schulzdean4@gmail.com>
2018-02-28 02:37:15 -0600

    shell sort

--- ShellSortDriver.cpp
+++ ShellSortDriver.cpp
@@ -2,7 +2,43 @@
 
 using namespace std;
 
-int main() {
-    cout << "hullo" << endl;
-    return 0;
+int main(int arc, char *argv[]) {
+	int *nums = new int[arc];
+
+	for (int i = 0; i < arc; i++) {
+		nums[i] = (int)argv[i];
+	}
+
+	int n = arc / sizeof(argv[0]);
+
+	cout << "Array before sorting: \n";
+	printArray(nums, n);
+
+	shellSort(nums, n);
+
+	cout << "\nArray after sorting: \n";
+	printArray(nums, n);
+
+	return 0;
+}
+void printArray(int nums[], int n)
+{
+	for (int i = 0; i < n; i++)
+		cout << nums[i] << " ";
+}
+int shellSort(int nums[], int n)
+{
+	for (int g = n / 2; g > 0; g /= 2)
+	{
+		for (int i = g; i < n; i += 1)
+		{
+			int temp = nums[i];
+			int k;
+			for (k = i; k >= g && nums[k - g] > temp; k -= g) {
+				nums[k] = nums[k - g];
+			}
+			nums[k] = temp;
+		}
+	}
+	return 0;
 }
