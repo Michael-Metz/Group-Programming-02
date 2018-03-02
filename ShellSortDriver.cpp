b4591290d05ec3d48f73b044ef70ce51713e9401
MIchael Metz <msmetz@ilstu.edu>
2018-03-02 16:33:10 -0600

    Updated ShellsortDriver

--- ShellSortDriver.cpp
+++ ShellSortDriver.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
 
 using namespace std;
 void printArray(int nums[], int n)
@@ -23,22 +24,63 @@ int shellSort(int nums[], int n)
 	}
 	return 0;
 }
+void printUsage(){
+	cout << "shellsort [input-file-path] [output-file-path]" << endl << endl;
+	cout << "This program reads an input file line by line parsing long values and then sorts them" << endl;
+	cout << "and stores them in the same format, using the output file path" << endl;
+
+}
 int main(int arc, char *argv[]) {
-	int *nums = new int[arc];
-
-	for (int i = 0; i < arc; i++) {
-		nums[i] = atoi(argv[i]);
+	//make sure two arguments are  passed when running the program from command line
+	if (arc != 3) {
+		printUsage();
+		return 0;
 	}
+	//at this point
+	//argv[1] is the path to the input file
+	//argv[2] is the path to save the output file
 
-	int n = arc / sizeof(argv[0]);
+	int totalNumbersRead = 0;
+	ifstream inputFile(argv[1]);
 
-	cout << "Array before sorting: \n";
-	printArray(nums, n);
+	if (!inputFile.is_open())
+	{
+		cout << "could not open file" << endl;
+	}
+	else
+	{
+		//read input file line by line and add to stack
+		long num;
+		while (inputFile >> num)
+		{
+			//TODO add num to stack
+			totalNumbersRead++;
+		}
+		inputFile.close();
 
-	shellSort(nums, n);
+		//now we know how many numbers we read from the file
+		int *numbers = new int[totalNumbersRead];
 
-	cout << "\nArray after sorting: \n";
-	printArray(nums, n);
+		//TODO add numbers from stack to the numbers array.
 
+
+
+		//sort
+		shellSort(numbers, totalNumbersRead);
+
+
+
+		//write sorted array to the output file
+		ofstream outputFile(argv[2]);
+		if(!outputFile.is_open()){
+			cout << "unable to open output file" << endl;
+			return;
+		}
+		for(int i = 0; i < totalNumbersRead; i++){
+			outputFile << numbers[i] << endl;
+		}
+
+		delete numbers;
+	}
 	return 0;
 }
